2a6c57ea3c7dba49da1f45587ed85d33fd32f109
Natasha <natashahinga58@gmail.com>
2024-11-08 08:49:42 +0300

    Code Drop

---
+++ djangojinjaapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def services(request):
+    return render(request, 'services.html')
+def base(request):
+    return render(request, 'base.html')
